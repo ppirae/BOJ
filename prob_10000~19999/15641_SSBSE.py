9171d1bbd89faf7bfd3b83c3c4763fdc29fe584b
Hyeongrae Jo <whgudfo77@naver.com>
2021-03-06 13:54:22 +0900

    BOJ

---
+++ prob_10000~19999/15641_SSBSE.py
@@ -0,0 +1,12 @@
+# 문제 이름
+# SUPER SUPER BINARY SEARCH DELUXE 2.5: THE LEGEND OF THE GOLDEN MAZASSUMNIDA,
+# EPISODE 2: THE MAZWAETL UNIVERSE,
+# PART 2: THE PARALLEL UNIVERSE AND THE LOST MAZASSUMNIDA: GAME OF THE YEAR EDITION
+
+
+# 문제 내용
+# 1 이상 100 이하의 자연수를 출력한다. 단, 하나의 자연수만 정답이다. 정답은 맞은 사람이 나타날 때마다 바뀐다.
+# 정답보다 작은 수를 출력하면 33% 부근에서 "틀렸습니다"를 받는다.
+# 정답보다 큰 수를 출력하면 66% 부근에서 "틀렸습니다"를 받는다.
+
+94
