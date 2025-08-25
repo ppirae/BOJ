68106b0732431261511e20e707f9e71478f31eb2
ppirae <whgudfo77@gmail.com>
2025-08-25 09:38:15 +0900

    BOJ

---
+++ prob_10000~19999/15655_N과 M(6).py
@@ -0,0 +1,19 @@
+def recur(number):
+    if number == M:
+        if arr == sorted(arr):
+            print(*arr)
+        return
+
+    for i in range(1, N+1):
+        if num_arr[i-1] in arr:
+            continue
+        arr.append(num_arr[i-1])
+        recur(number+1)
+        arr.pop()
+
+
+N, M = map(int, input().split())
+num_arr = sorted(list(map(int, input().split())))
+arr = []
+
+recur(0)
